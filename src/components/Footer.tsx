089af6665cfa32cec2e66e79eb8786f0159d3b67
ZheH <3986187325813355-ZheH@noreply.coze.cn>
2026-08-29 10:12:06 +0800

    fix: 缩小Footer天猫图标尺寸与小红书红色区域一致
    Coze-Commit-Type: user
    Coze-User-ID: 3986187325813355
    Coze-Conversation-ID: 11312581

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -81,9 +81,9 @@ export default function Footer() {
                 <img
                   src="/icon-tmall.png"
                   alt="Tmall"
-                  width={36}
-                  height={36}
-                  className="h-9 w-9 rounded-md object-contain"
+                  width={28}
+                  height={28}
+                  className="h-7 w-7 rounded-md object-contain"
                 />
               </a>
               <a
